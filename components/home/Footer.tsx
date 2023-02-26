9618eff46e52835098a7415d17f9517f49f420f0
BlueBoxGamingGC <100632150+BlueBoxGamingGC@users.noreply.github.com>
2023-02-25 18:08:30 -0700

    Update Footer.tsx

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -30,36 +30,6 @@ const SectionLink: FC<SectionLinkProps> = ({ name, href }) => (
   </Anchor>
 )
 
-const developerSectionLinks = [
-  {
-    name: 'Docs',
-    href: 'https://docs.reservoir.tools/docs',
-  },
-  {
-    name: 'API Reference',
-    href: 'https://docs.reservoir.tools/reference/overview',
-  },
-  {
-    name: 'Github',
-    href: 'https://github.com/reservoirprotocol',
-  },
-]
-
-const companySectionLinks = [
-  {
-    name: 'Jobs',
-    href: 'https://jobs.ashbyhq.com/reservoir',
-  },
-  {
-    name: 'Terms of Use',
-    href: 'https://reservoir.tools/terms',
-  },
-  {
-    name: 'Privacy Policy',
-    href: 'https://reservoir.tools/privacy',
-  },
-]
-
 export const Footer = () => {
   return (
     <Flex
@@ -77,30 +47,16 @@ export const Footer = () => {
         },
       }}
     >
-      <Flex css={{ gap: 80, '@bp600': { gap: 136 } }}>
-        <Flex direction="column">
-          <SectionTitle title="Developers" />
-          {developerSectionLinks.map((props) => (
-            <SectionLink key={props.name} {...props} />
-          ))}
-        </Flex>
-        <Flex direction="column">
-          <SectionTitle title="Company" />
-          {companySectionLinks.map((props) => (
-            <SectionLink key={props.name} {...props} />
-          ))}
-        </Flex>
-      </Flex>
-      <Flex
+       <Flex
         direction="column"
         css={{ alignItems: 'flex-start', '@bp600': { alignItems: 'flex-end' } }}
       >
-        <SectionTitle title="Join Reservoir Community" />
+        <SectionTitle title="Join BlueBox Gaming Community" />
         <Flex css={{ gap: '$4', mt: 16 }}>
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://twitter.com/reservoir0x"
+            href="https://twitter.com/BlueBoxGC"
           >
             <Button size="xs" color="gray3">
               <FontAwesomeIcon icon={faTwitter} width={14} height={14} />
